bacbf13646a8fe2cb0bfd1c825f8c305bfe5128f
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-22 14:11:53 -0700

    added ender pearl icon and set stack size to 16

--- ender-pearl.lua
+++ ender-pearl.lua
@@ -1,7 +1,10 @@
 local enderPearl = {
   type = "capsule",
   name = "ender-pearl",
-  capsule_action = "throw"
+  capsule_action = "throw",
+  icon = "graphics/Ender_Pearl_JE3_BE2.png",
+  icon_size = 160,
+  stack_size = 16
 }
 
 data:extend({
