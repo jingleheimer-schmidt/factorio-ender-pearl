4ae856058ba54b07dc84f180961ef6a3f80c021d
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-22 13:59:00 -0700

    first attempt at adding ender pearl capsule and control.lua script thing

--- control.lua
+++ control.lua
@@ -1,1 +1,10 @@
-aaaaaaaaaaa
+function on_player_used_capsule(player, ender-pearl)
+  local position = ender-pearl.surface.find_non_colliding_position("character", ender-pearl.position, 128, 2)
+
+  if position then
+      player.print("Nice throw! Total distance: " .. distance(player.position, position))
+      player.teleport(position, ender-pearl.surface)
+  else
+      player.print("Teleport failed: No valid locations nearby")
+  end
+end
---
+++ ender-pearl.lua
@@ -0,0 +1,9 @@
+local enderPearl = {
+  type = "capsule",
+  name = "ender-pearl",
+  capsule_action = "throw"
+}
+
+data:extend({
+  enderPearl
+})
