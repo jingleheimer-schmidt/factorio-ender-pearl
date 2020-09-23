188e515e97847f7e8091550c173026937aa67bd1
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-23 15:48:36 -0700

    still confused

--- control.lua
+++ control.lua
@@ -1,8 +1,8 @@
 
 script.on_event(defines.events.on_script_trigger_effect, function(event)
 
-  -- adds the player as a variable
-  local player = game.players[game.player_index]
+  -- adds the player as a variable (?)
+  local player = game.players[event.player_index]
 
   -- adds the ender pearl position as a variable (?)
   local enderpearl_position = event.target_position
@@ -49,6 +49,6 @@ function teleport_player_to_enderpearl(player, enderpearl_position)
   end
 end
 
-function distance(position_1, position_2)
-    return math.floor(((position_1.x - position_2.x) ^ 2 + (position_1.y - position_2.y) ^ 2) ^ 0.5)
-end
+-- function distance(position_1, position_2)
+--     return math.floor(((position_1.x - position_2.x) ^ 2 + (position_1.y - position_2.y) ^ 2) ^ 0.5)
+-- end
