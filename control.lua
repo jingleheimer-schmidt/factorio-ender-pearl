cfcfe7b5e4a898bfc140bcddc32fc6e998825418
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-22 17:16:22 -0700

    added distance function

--- control.lua
+++ control.lua
@@ -8,3 +8,7 @@ function on_player_used_capsule(player, ender-pearl)
       player.print("Teleport failed: No valid locations nearby")
   end
 end
+
+function distance(position_1, position_2)
+    return math.floor(((position_1.x - position_2.x) ^ 2 + (position_1.y - position_2.y) ^ 2) ^ 0.5)
+end
