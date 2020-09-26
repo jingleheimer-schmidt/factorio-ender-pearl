118c5f07125132b99561602ca728a9632a8c7b11
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-26 04:25:50 -0700

    player now takes damage = ~88% of distance traveled

--- control.lua
+++ control.lua
@@ -2,28 +2,37 @@
 -- runs when a script is triggered
 script.on_event(defines.events.on_script_trigger_effect, function(event)
 
-  -- adds the ender pearl position as a variable (?)
+  -- adds the ender pearl position as a variable
   local enderpearl_position = event.target_position
 
   -- adds the player as a variable
   local player = event.source_entity
 
+  -- adds player position as a variable
+  local player_position = event.source_position
+
   -- finds a non-coolliding position within 2 tiles of target
   local valid_position = player.surface.find_non_colliding_position("character", enderpearl_position, 2, 1)
 
-  -- subtracts 10 from player health and saves as variable
-  local ouch = player.health-10
+  -- calculates distance between player and ender peal target
+  local distance = math.floor(((player_position.x - enderpearl_position.x) ^ 2 + (player_position.y - enderpearl_position.y) ^ 2) ^ 0.5)
 
-  -- checks if capsule is an enderpearl, then calls teleport function
+  -- calculates a rancom variable between .91 and 1.09
+  local almost = (math.random(91, 109))*.01
+
+  -- subtracts ~88% of distance traveled from player health
+  local ouch = player.health-distance*(.88*almost)
+
+  -- checks if capsule is an enderpearl, then teleports player to pearl
   if event.effect_id == "ender_pearl_effect_id" then
     if valid_position then
       -- teleports player to ender pearl
       player.teleport(valid_position)
-      -- if player health will be at or below zero then kill the entity
+      -- if player health will be at or below zero then kill the them
 		  if ( ouch <= 0 ) then
 			     player.die()
-		  else
-			-- otherwise subtract 10 from player health
+      -- otherwise subtract ~88% of distance traveled from player health
+      else
 			   player.health = ouch
 		  end
     else
