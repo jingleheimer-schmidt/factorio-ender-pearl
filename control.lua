0db6caa0220cd1e7c5b2c5217db9b14c80e7d524
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-26 00:04:34 -0700

    player now takes 10 damage per throw. can be lethal

--- control.lua
+++ control.lua
@@ -11,10 +11,21 @@ script.on_event(defines.events.on_script_trigger_effect, function(event)
   -- finds a non-coolliding position within 2 tiles of target
   local valid_position = player.surface.find_non_colliding_position("character", enderpearl_position, 2, 1)
 
+  -- subtracts 10 from player health and saves as variable
+  local ouch = player.health-10
+
   -- checks if capsule is an enderpearl, then calls teleport function
   if event.effect_id == "ender_pearl_effect_id" then
     if valid_position then
+      -- teleports player to ender pearl
       player.teleport(valid_position)
+      -- if player health will be at or below zero then kill the entity
+		  if ( ouch <= 0 ) then
+			     player.die()
+		  else
+			-- otherwise subtract 10 from player health
+			   player.health = ouch
+		  end
     else
     player.player.print("Teleport failed: No valid locations nearby")
     end
