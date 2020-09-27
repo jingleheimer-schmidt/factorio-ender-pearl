73f7eee313c41bccbeb5153c2ba6ad27930deae6
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-09-26 20:29:55 -0700

    adjusted distance formula

--- control.lua
+++ control.lua
@@ -11,11 +11,11 @@ script.on_event(defines.events.on_script_trigger_effect, function(event)
   -- adds player position as a variable
   local player_position = event.source_position
 
-  -- finds a non-coolliding position within 2 tiles of target
+  -- finds a non-colliding position within 2 tiles of target
   local valid_position = player.surface.find_non_colliding_position("character", enderpearl_position, 2, 1)
 
   -- calculates distance between player and ender peal target
-  local distance = math.floor(((player_position.x - enderpearl_position.x) ^ 2 + (player_position.y - enderpearl_position.y) ^ 2) ^ 0.5)
+  local distance = math.floor(((player_position.x - valid_position.x) ^ 2 + (player_position.y - valid_position.y) ^ 2) ^ 0.5)
 
   -- calculates a rancom variable between .91 and 1.09
   local almost = (math.random(91, 109))*.01
@@ -36,12 +36,30 @@ script.on_event(defines.events.on_script_trigger_effect, function(event)
 			   player.health = ouch
 		  end
     else
-      play_sound{
-        path = "__factorio-ender-pearl__/sounds/enderpearl-failed.ogg", position = enderpearl_position, volume_modifier = 1}
       player.player.print("Teleport failed: No valid locations nearby")
     end
   else
     return
   end
+
+  -- -- checks for if enderpearl failed, then plays sound
+  -- if event.effect_id == "ender_pearl_failed" then
+  --   if valid_position == nil then
+  --     game.play_sound{
+  --       path = "__factorio-ender-pearl__/sounds/enderpearl-failed.ogg", position = valid_position, volume_modifier = 1}
+  --   else
+  --     return
+  --   end
+  -- else
+  --   return
+  -- end
+  --
+  -- -- check if enderpearl has been thrown, then plays thorw sound
+  -- if event.effect_id == "ender_pearl_launched" then
+  --   game.play_sound{
+  --     path = "__factorio-ender-pearl__/sounds/enderpearl-launched.ogg", position = player_position, volume_modifier = 1}
+  -- else
+  --   return
+  -- end
 
 end)
