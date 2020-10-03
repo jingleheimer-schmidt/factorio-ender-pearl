0576bffa1a90f4f5babc6d4bc68bbd5305986294
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-02 23:40:55 -0700

    added damage sounds

---
+++ damage-sounds.lua
@@ -0,0 +1,26 @@
+local enderpearlFailedLanding = {
+  name = "ender-pearl-failed-landing",
+  type = "sound",
+  filename = "__factorio-ender-pearl__/sounds/enderpearl-failed.ogg",
+  volume = 1
+}
+
+local fallSmall = {
+  name = "fall-small",
+  type = "sound",
+  filename = "__factorio-ender-pearl__/sounds/fallsmall.ogg",
+  volume = 1
+}
+
+local fallBig = {
+  name = "fall-big",
+  type = "sound",
+  filename = "__factorio-ender-pearl__/sounds/fallbig.ogg",
+  volume = 1
+}
+
+data:extend({
+  enderpearlFailedLanding,
+  fallSmall,
+  fallBig
+})
