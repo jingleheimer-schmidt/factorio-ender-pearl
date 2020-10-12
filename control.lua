69cce317653e2f3db068bebb1dd02027d48286d0
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2020-10-12 16:24:18 -0700

    cleaned up teleporting code, now uses functions!

--- control.lua
+++ control.lua
@@ -1,35 +1,38 @@
+-- finds a non-colliding position within 2 tiles of target
+function find_valid_position(player, enderpearl_position)
+  -- check if player can fit within 2 tiles of ender pearl position
+  local valid_position = player.surface.find_non_colliding_position("character", enderpearl_position, 2, 1)
+  return valid_position
+end
+
+-- calculate ouch factor
+function calculate_ouch(player_position, valid_position)
+  -- calculates distance between player and valid_position
+  local distance = math.floor(((player_position.x - valid_position.x) ^ 2 + (player_position.y - valid_position.y) ^ 2) ^ 0.5)
+  -- calculates a random variable between .91 and 1.09
+  local almost = (math.random(91, 109))*.01
+  -- sets ouch as ~88% of distance traveled
+  local ouch = distance*(.88*almost)
+  return ouch
+end
+
 -- runs when a script is triggered
 script.on_event(defines.events.on_script_trigger_effect, function(event)
-
-  -- adds the ender pearl position as a variable
-  local enderpearl_position = event.target_position
-
-  -- adds the player as a variable
-  local player = event.source_entity
-
-  -- adds player position as a variable
-  local player_position = event.source_position
-
-  -- finds a non-colliding position within 2 tiles of target
-  local valid_position = player.surface.find_non_colliding_position("character", enderpearl_position, 2, 1)
-
-  -- calculates distance between player and ender peal target
-  local distance = math.floor(((player_position.x - enderpearl_position.x) ^ 2 + (player_position.y - enderpearl_position.y) ^ 2) ^ 0.5)
-
-  -- calculates a rancom variable between .91 and 1.09
-  local almost = (math.random(91, 109))*.01
-
-  -- saves health deduction
-  local health_deduction = distance*(.88*almost)
-
-  -- subtracts ~88% of distance traveled from player health
-  local ouch = player.health-health_deduction
-
-  -- checks if capsule is an enderpearl, then teleports player to pearl
+  -- checks if script is triggered by an enderpearl
   if event.effect_id == "ender_pearl_effect_id" then
+    -- adds the ender pearl target position as a variable
+    local enderpearl_position = event.target_position
+    -- adds the player as a variable
+    local player = event.source_entity
+    -- adds player position as a variable
+    local player_position = event.source_position
+    -- finds a non-colliding position within 2 tiles of target
+    local valid_position = find_valid_position(player, enderpearl_position)
     if valid_position then
       -- teleports player to ender pearl
       player.teleport(valid_position)
+      -- calculate ouch factor
+      local ouch = calculate_ouch(player_position, valid_position)
       -- if player health will be at or below zero then kill the them
 		  if ( ouch <= 0 ) then
            game.play_sound{
@@ -37,8 +40,8 @@ script.on_event(defines.events.on_script_trigger_effect, function(event)
            player.die()
       -- otherwise subtract ~88% of distance traveled from player health
       else
-			   player.health = ouch
-         if ( health_deduction <= 16) then
+			   player.health = player.health - ouch
+         if ( ouch <= 16) then
            game.play_sound{
              path = "fall-small", position = valid_position, volume_modifier = 1}
          else
@@ -54,5 +57,4 @@ script.on_event(defines.events.on_script_trigger_effect, function(event)
   else
     return
   end
-
 end)
